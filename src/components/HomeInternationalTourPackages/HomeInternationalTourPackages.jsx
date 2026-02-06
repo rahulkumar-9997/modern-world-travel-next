b087b2cdb4c2be71334d37798ff062e8c48c8823
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-02-06 13:21:48 +0530

    06_02_2026_internation_tour_package

--- src/components/HomeInternationalTourPackages/HomeInternationalTourPackages.jsx
+++ src/components/HomeInternationalTourPackages/HomeInternationalTourPackages.jsx
@@ -31,7 +31,7 @@ export function HomeInternationalTourPackages({ initialData }) {
                         <div key={tour.nid} className="w-full h-full shadow-lg hover:shadow-2xl rounded-12">
                             <Link href=
                                 {`/tour-package/${tour.url}`}
-                                className="tourCard -type-1 d-block border bg-white hover-shadow-1 overflow-hidden rounded-12 hover-shadow h-full! group flex! flex-col! justify-between!"
+                                className="tourCard -type-1 d-block border bg-white hover-shadow-1 overflow-hidden rounded-12 hover-shadow h-full! group flex! flex-col!"
                             >
                                 <div className="tourCard__header">
                                     <div className="tourCard__image -hover-image-scale__image ratio ratio-28:20">
@@ -43,30 +43,31 @@ export function HomeInternationalTourPackages({ initialData }) {
                                             height={200}
                                         />
                                     </div>
-
-                                </div>
-                                <div className="international-tour-card">
-                                    <div className="international-tour-section">
-                                        <div className="tourCard__content flex! flex-col! justify-between! gap-2!">
-                                            {/* <div className="tourCard__location d-flex items-center text-13 text-light-2">
-                                                <i className="icon-pin d-flex text-16 text-light-2 mr-5" />
-                                                {tour.location}
-                                            </div> */}
-                                            <Heading
-                                                level={5}
-                                                text={tour.title}
-                                                className="ttourCard__title  fw-500 mt-5 leading-tight! "
-                                            />
-                                            <p className='relative z-10! line-clamp-4 group-hover:text-white! transition-colors'>
-                                                {tour.description}
-                                            </p>                                    
+                                </div>                                
+                                <div className="international-tour-card w-full h-full">
+                                    <div className="flex! flex-col! justify-between! w-full h-full">
+                                        <div className="international-tour-section">
+                                            <div className="tourCard__content flex! flex-col! justify-between! gap-2!">
+                                                {/* <div className="tourCard__location d-flex items-center text-13 text-light-2">
+                                                    <i className="icon-pin d-flex text-16 text-light-2 mr-5" />
+                                                    {tour.location}
+                                                </div> */}
+                                                <Heading
+                                                    level={5}
+                                                    text={tour.title}
+                                                    className="ttourCard__title  fw-500 mt-5 leading-tight! "
+                                                />
+                                                <p className='relative z-10! line-clamp-4 group-hover:text-white! transition-colors'>
+                                                    {tour.description}
+                                                </p>                                    
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className='international-tour-section-in'>
-                                        <div className="d-flex justify-between items-center home-duration">
-                                            <div className="d-flex items-center duration-content">
-                                                <i className="icon-clock text-16 mr-5" />
-                                                {tour.duration}
+                                        <div className='international-tour-section-in'>
+                                            <div className="d-flex justify-between items-center home-duration">
+                                                <div className="d-flex items-center duration-content">
+                                                    <i className="icon-clock text-16 mr-5" />
+                                                    {tour.duration}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
