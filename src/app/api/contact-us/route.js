a43ad6b18ae4aeecd426bfe1312ca69dc4a6360f
akshatwizard <akshat@wizards.co.in>
2026-05-26 17:11:56 +0530

    Update route.js

--- src/app/api/contact-us/route.js
+++ src/app/api/contact-us/route.js
@@ -26,8 +26,8 @@ export async function POST(request) {
       />,
     );
     const { error } = await resend.emails.send({
-      from: "Modern World Travel <seo@modernworldtravel.com>",
-      to: ["info@modernworldtravel.com", "modernworldtravel@gmail.com"],
+      from: "Modern World Travel <mwtseo@wizards.co.in>",
+      to: ["info@modernworldtravel.com", "modernworldtravel@gmail.com", "akshat@wizards.co.in"],
       replyTo: email,
       subject: `New Contact Form Submission - ${name}`,
       html: emailHtml,
